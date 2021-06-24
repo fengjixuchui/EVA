5bc1146cf5dc1c8ac48faf274018e08365265688
ORCA666 <mohammadalisareeni@gmail.com>
2021-06-24 10:15:49 +0300

    Add files via upload

---
+++ EVA/connector.h
@@ -0,0 +1,17 @@
+#ifndef CONNECTOR_H	
+#define CONNECTOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	int find();
+	int find2();
+	  
+#ifdef __cplusplus
+
+}
+#endif // __cplusplus
+
+#endif // CONNECTOR_H	
+
---
+++ EVA/find-PID.cpp
@@ -0,0 +1,53 @@
+//http://www.cplusplus.com/forum/windows/12137/
+
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <Tlhelp32.h>
+#include "connector.h"
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+    DWORD FindProcessId(const std::wstring& processName);
+
+
+    int find() {
+        std::wstring processName = L"explorer.exe";
+        DWORD processID = FindProcessId(processName);
+        return processID;
+    }
+    int find2() {
+        std::wstring processName = L"chrome.exe";
+        DWORD processID = FindProcessId(processName);
+        return processID;
+    }
+
+    DWORD FindProcessId(const std::wstring& processName) {
+        PROCESSENTRY32 processInfo;
+        processInfo.dwSize = sizeof(processInfo);
+        HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+        if (processesSnapshot == INVALID_HANDLE_VALUE)
+            return 0;
+        Process32First(processesSnapshot, &processInfo);
+        if (!processName.compare(processInfo.szExeFile))
+        {
+            CloseHandle(processesSnapshot);
+            return processInfo.th32ProcessID;
+        }
+        while (Process32Next(processesSnapshot, &processInfo))
+        {
+            if (!processName.compare(processInfo.szExeFile))
+            {
+                CloseHandle(processesSnapshot);
+                return processInfo.th32ProcessID;
+            }
+        }
+        CloseHandle(processesSnapshot);
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
